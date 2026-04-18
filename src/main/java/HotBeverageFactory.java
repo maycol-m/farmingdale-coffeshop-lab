024414f1c7fa52f66c5f5be7883fa34e7667eff2
medin <medinamaycol1@gmail.com>
2026-04-18 16:47:59 -0400

    Update hot and iced beverage factories

--- src/main/java/HotBeverageFactory.java
+++ src/main/java/HotBeverageFactory.java
@@ -1,2 +1,21 @@
 public class HotBeverageFactory implements BeverageFactory {
+    @Override
+    public Beverage createCoffee() {
+        return new Coffee("Hot");
+    }
+
+    @Override
+    public Beverage createLatte() {
+        return new Latte("Hot");
+    }
+
+    @Override
+    public Beverage createCappuccino() {
+        return new Cappuccino("Hot");
+    }
+
+    @Override
+    public Beverage createTea() {
+        return new Tea("Hot");
+    }
 }
--- src/main/java/IcedBeverageFactory.java
+++ src/main/java/IcedBeverageFactory.java
@@ -1,2 +1,21 @@
 public class IcedBeverageFactory implements BeverageFactory {
+    @Override
+    public Beverage createCoffee() {
+        return new Coffee("Iced");
+    }
+
+    @Override
+    public Beverage createLatte() {
+        return new Latte("Iced");
+    }
+
+    @Override
+    public Beverage createCappuccino() {
+        return new Cappuccino("Iced");
+    }
+
+    @Override
+    public Beverage createTea() {
+        return new Tea("Iced");
+    }
 }
